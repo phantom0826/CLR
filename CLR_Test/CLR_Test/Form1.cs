2e3e050c8f8fb44d5b3f7784e9e1a0b41dff2621
june <june08266@naver.com>
2017-11-10 22:41:47 +0900

    오버플로우 확인

---
+++ CLR_Test/CLR_Test/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CLR_Test
+{
+    public partial class Form1 : Form
+    {
+        byte b = 100;
+
+        public Form1()
+        {
+            InitializeComponent();
+            b = checked((byte)(b + 155));
+            unchecked
+            {
+                byte c = 200;
+                b += c;
+            }
+            //checked
+            {
+                test();
+            }
+            
+            Console.WriteLine(b);
+        }
+        private void test()
+        {
+            //checked
+            {
+                byte tt = 200;
+                tt += 66;
+                Console.WriteLine(tt);
+            }
+        }
+    }
+}
